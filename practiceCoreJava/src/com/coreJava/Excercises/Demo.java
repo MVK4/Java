d044f4b4fff4d1061104618803011b1deb7bfdf6
mvk <mvk@10.0.0.126>
2017-12-06 21:25:14 -0800

    Moved from display numbers

---
+++ practiceCoreJava/src/com/coreJava/Excercises/Demo.java
@@ -0,0 +1,14 @@
+package com.coreJava.Excercises;
+
+public class Demo {
+	public static void main(String[] args) {
+		int x = 1;
+		do {
+			System.out.println(x);
+			x++;
+			/////
+			/////
+			////////
+		}while(x<=10);
+	}
+}
